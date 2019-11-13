239242543554ff881b48da936054ec97bf66a209
Druid3000 <sarvarov.s@mail.ru>
2019-11-14 01:08:21 +0300

    Make Sonar failed

--- src/main/java/org/springframework/samples/petclinic/Test.java
+++ src/main/java/org/springframework/samples/petclinic/Test.java
@@ -2,6 +2,15 @@ package org.springframework.samples.petclinic;
 
 public class Test {
     public static void main(String[] args) {
+        String expression = "z;
+        switch(expression) {
+            case "x":
+                // code block
+                break;
+            case "y":
+                // code block
+                break;
 
+        }
     }
 }
