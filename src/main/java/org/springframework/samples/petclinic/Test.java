0e6bb186d93845d4b9d49355afb21719c2bf442b
Druid3000 <sarvarov.s@mail.ru>
2019-11-14 01:25:54 +0300

    Make Sonar ok

--- src/main/java/org/springframework/samples/petclinic/Test.java
+++ src/main/java/org/springframework/samples/petclinic/Test.java
@@ -2,15 +2,6 @@ package org.springframework.samples.petclinic;
 
 public class Test {
     public static void main(String[] args) {
-        String expression = "z";
-        switch(expression) {
-            case "x":
-                // code block
-                break;
-            case "y":
-                // code block
-                break;
 
-        }
     }
 }
